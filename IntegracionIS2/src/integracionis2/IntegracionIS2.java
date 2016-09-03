14c5a6253dd70d08fd53027bc82dfa43e5611134
Usuario <Usuario@DESKTOP-RUQICM2>
2016-09-03 12:42:59 -0500

    AGREGANDO MENU

--- IntegracionIS2/src/integracionis2/IntegracionIS2.java
+++ IntegracionIS2/src/integracionis2/IntegracionIS2.java
@@ -5,6 +5,8 @@
  */
 package integracionis2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
@@ -17,12 +19,19 @@ public class IntegracionIS2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
-        
+        Scanner ingreso=new Scanner(System.in);
         int galones, opciones;
         String tipo = null;
         double diesel=1.50, gasolina=3.00,precio=0;
         double iva=12,calculoiva = 0;
         double total = 0,subtotal = 0;
+        
+        System.out.println("Gasolinera");
+        System.out.println("Bienvenidos a PetroEcuador");
+        System.out.println("1)Gasolina $3.00 \n2)Diesel   $1.50");
+        opciones=ingreso.nextInt();
+        System.out.println("ingrese la cantidad en Galones");
+        galones=ingreso.nextInt();
     }
     
 }
